3f80c748c5da3e627828a3183af9d9e38d87b487
Mizakson <maxwellizakson@gmail.com>
2024-05-21 10:33:06 -0400

    Pass first letter capitalized test

--- src-test/capitalize.test.js
+++ src-test/capitalize.test.js
@@ -5,3 +5,8 @@ test("type is string", () => {
     const a = capitalize("max")
     expect(typeof a).toBe("string");
 })
+
+test("is first letter capitalized", () => {
+    const a = capitalize("max")
+    expect(a[0]).toBe("M")
+})
