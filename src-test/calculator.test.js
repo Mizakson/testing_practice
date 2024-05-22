d2580e462c9c54bfbc12ce08981d46c66b4e9535
Mizakson <maxwellizakson@gmail.com>
2024-05-22 10:29:37 -0400

    Pass tests to check if output is a number For subtract, divide, and multiply functions

--- src-test/calculator.test.js
+++ src-test/calculator.test.js
@@ -1,5 +1,17 @@
 import calculator from "../src/calculator";
 
-test("add", () => {
+test("add -> int", () => {
     expect(typeof calculator.add(1,1)).toBe("number")
+})
+
+test("subtract -> int", () => {
+    expect(typeof calculator.subtract(4,2)).toBe("number")
+})
+
+test("divide -> int", () => {
+    expect(typeof calculator.divide(6,3)).toBe("number")
+})
+
+test("multiply -> int", () => {
+    expect(typeof calculator.multiply(2,5)).toBe("number")
 })
