4af7614eafbc02e30000e88f34064f5825f06b3b
Mizakson <maxwellizakson@gmail.com>
2024-05-23 12:35:56 -0400

    Add analyzeArray function body

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,3 +1,22 @@
 export default function analyzeArr(arr) {
+    let len = arr.length
     
+    // average
+    let sum = 0
+    for (let i = 0; i < len; i++) {
+        sum += arr[i]
+    }
+    let avg = Math.floor(sum / len) 
+
+    // min and max
+    let minimum = Math.min(...arr)
+    let maximum = Math.max(...arr)
+
+    return {
+        average: avg,
+        min: minimum,
+        max: maximum,
+        length: len
+    }
+
 }
